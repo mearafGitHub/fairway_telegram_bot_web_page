3f17cc8df277fb05b83cfb45c2a3b647d0e32ccc
mearaf <mearaftadewos@gmail.com>
2023-01-16 12:55:18 +0300

    second

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,8 +1,8 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN =  "5831288377:AAGZs1UuWdc7nXQUZzQtHbS_EyBWpsVsgeo" // "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://reliable-squirrel-d78bf6.netlify.app/"; //"https://celebrated-torte-184681.netlify.app/"
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
