ed57807ccecc9089f06ed1a34fa74900be8aa394
mearaf <mearaftadewos@gmail.com>
2023-01-18 12:45:15 +0300

    bot check

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,12 +2,12 @@ const { Telegraf } = require("telegraf");
 const TOKEN =  "5831288377:AAGZs1UuWdc7nXQUZzQtHbS_EyBWpsVsgeo"; // "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI"
 const bot = new Telegraf(TOKEN);
 
-const fairway_bot_link = "http://localhost:3000/"; // "https://quiet-beijinho-483168.netlify.app/"; 
+const fairway_bot_web_link = "https://shiny-baklava-4099b1.netlify.app/"; // 
 console.log("Bot Satrted")
 bot.start((ctx) =>
   ctx.reply("Hi, 😊 Welcome to Fairway!", {
     reply_markup: {
-      keyboard: [[{ text: "Fairway Menu", web_app: { url: fairway_bot_link } }]],
+      keyboard: [[{ text: "Fairway Menu", web_app: { url: fairway_bot_web_link } }]],
     },
   })
 );
